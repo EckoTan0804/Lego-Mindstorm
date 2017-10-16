2744ca7e99be04ff8de1077955e18a25ea91d9a2
EckoTan0804 <ecko0804@gmail.com>
2017-10-16 10:51:31 +0200

    Hello World! Test the connection with the brick

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package test;
+import lejos.hardware.Button;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Button.waitForAnyPress();
+    }
+}
