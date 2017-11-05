ac0e87fd3adf28f3a449c03d162faf3963901069
EckoTan0804 <ecko0804@gmail.com>
2017-11-05 10:53:11 +0100

    improved: call static method getMissionStringList to create menu list instead of create a string array

--- src/mission/MissionMenu.java
+++ src/mission/MissionMenu.java
@@ -5,8 +5,9 @@ import lejos.utility.TextMenu;
 public class MissionMenu extends TextMenu {
 
 	private static final String MENU_TITLE = "Parcours";
-	private static final String[] missions = { Mission.LINE_FOLLOWING.getMission(), Mission.LABYRINTH.getMission(),
-			Mission.OBSTACLE_SHIFTING.getMission(), Mission.BRIDGE.getMission() };
+//	private static final String[] missions = { Mission.LINE_FOLLOWING.getMission(), Mission.LABYRINTH.getMission(),
+//			Mission.OBSTACLE_SHIFTING.getMission(), Mission.BRIDGE.getMission() };
+	private static final String[] missions = Mission.getMissionStringList();
 	
 	public static final int MENU_ITEM_LINE_FOLLOWING = 0;
 	public static final int MENU_ITEM_LABYRINTH = 1;
