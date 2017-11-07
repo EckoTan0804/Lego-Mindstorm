d567fff14301136942fe91717eedac8c96dff41b
EckoTan0804 <ecko0804@gmail.com>
2017-11-07 21:48:34 +0100

    created Driva.java for controlling the robot's move

---
+++ src/ev3Robot/Drive.java
@@ -0,0 +1,100 @@
+package ev3Robot;
+
+import lejos.robotics.RegulatedMotor;
+import lejos.robotics.navigation.DifferentialPilot;
+
+public class Drive {
+
+	private DifferentialPilot pilot;
+	private Robot robot;
+
+	public Drive(double wheelDiameter, double trackWidth, RegulatedMotor leftMotor, RegulatedMotor rightMotor) {
+		this.pilot = new DifferentialPilot(wheelDiameter, trackWidth, leftMotor, rightMotor);
+	}
+
+	public Drive(Robot robot) {
+		this.robot = robot;
+		this.pilot = new DifferentialPilot(robot.getWHEEL_DIAMETER(), robot.getTRACK_WIDTH(), robot.getLeftMotor(),
+				robot.getRightMotor());
+	}
+
+	public void setSpeed(double speed) {
+		this.pilot.setTravelSpeed(speed);
+	}
+
+	public void goForwardWithPilot() {
+		this.pilot.forward();
+	}
+
+	public void goBackwardWithPilot() {
+		this.pilot.backward();
+	}
+
+	public void goForwardWithMotors() {
+		this.robot.getLeftMotor().forward();
+		this.robot.getRightMotor().forward();
+	}
+
+	public void goBackwardWithMotors() {
+		this.robot.getLeftMotor().backward();
+		this.robot.getRightMotor().backward();
+	}
+
+	public void setMotorSpeed(float leftMotorTargetSpeed, float rightMotorTargetSpeed) {
+		this.robot.getLeftMotor().setSpeed(leftMotorTargetSpeed);
+		this.robot.getRightMotor().setSpeed(rightMotorTargetSpeed);
+	}
+
+	/**
+	 * Moves the robot a specific distance (unit: cm) in an (hopefully) straight
+	 * line.
+	 * 
+	 * @param distance
+	 *            A positive distance causes forward motion, a negative distance
+	 *            moves backward.
+	 */
+	public void travel(double distance) {
+		this.pilot.travel(distance);
+	}
+
+	/**
+	 * Moves the robot a specified distance along an arc of specified radius, after
+	 * which the robot stops moving. The units (inches, cm) for distance must be the
+	 * same as the units used for radius.
+	 * 
+	 * @param radius
+	 *            If radius is positive, the robot arcs left, and the center of the
+	 *            turning circle is on the left side of the robot. If radius is
+	 *            negative, the robot arcs right, and the center of the turning
+	 *            circle is on the right side of the robot. If radius is zero, the
+	 *            robot rotates in place
+	 * @param distance
+	 *            The robot will stop when it has moved along the arc distance
+	 *            units. If distance is positive, the robot will move travel
+	 *            forwards. If distance is negative, the robot will move travel
+	 *            backwards. If distance is zero, the robot will not move and the
+	 *            method returns immediately.
+	 */
+	public void travelArc(double radius, double distance) {
+		this.pilot.travelArc(radius, distance, true);
+	}
+
+	public boolean isMoving() {
+		return this.pilot.isMoving();
+	}
+
+	public void stop() {
+		this.pilot.stop();
+	}
+
+	/**
+	 * 
+	 * @param angle
+	 *            The wanted angle of rotation in degrees. Positive angle rotate
+	 *            left (anti-clockwise), negative right.
+	 */
+	public void rotate(double angle) {
+		this.pilot.rotate(angle, true);
+	}
+
+}
--- src/ev3Robot/Robot.java
+++ src/ev3Robot/Robot.java
@@ -19,28 +19,31 @@ import sensor.SingleValueSensorWrapper;
 public class Robot implements Runnable {
 
 	// Date need to be measured and adjusted
-	private static final double WHEEL_DIAMETER = 3.2d; // unit:cm
-	private static final double TRACK_WIDTH = 11d; // unit: cm
-	private static final Port LEFT_MOTOR = MotorPort.A;
-	private static final Port RIGHT_MOTOR = MotorPort.D;
+	private final double WHEEL_DIAMETER = 3.2d; // unit:cm
+	private final double TRACK_WIDTH = 11d; // unit: cm
+	private final Port LEFT_MOTOR = MotorPort.A;
+	private final Port RIGHT_MOTOR = MotorPort.D;
 
 	private static final Port COLOR_SENSOR = SensorPort.S1;
 	private static final Port TOUCH_SENSOR_1 = SensorPort.S2;
 	private static final Port TOUCH_SENSOR_2 = SensorPort.S3;
 	private static final Port ULTRASONIC_SENSOR = SensorPort.S4;
 
-	private DifferentialPilot pilot;
+	// private DifferentialPilot pilot;
 	private SensorThread sensors;
 	private MissionMenu missionMenu;
 
 	private EV3LargeRegulatedMotor leftMotor;
 	private EV3LargeRegulatedMotor rightMotor;
 
+	private Drive drive;
+
 	public Robot() {
 
 		this.leftMotor = new EV3LargeRegulatedMotor(LEFT_MOTOR);
 		this.rightMotor = new EV3LargeRegulatedMotor(RIGHT_MOTOR);
-		this.pilot = new DifferentialPilot(WHEEL_DIAMETER, TRACK_WIDTH, leftMotor, rightMotor, false);
+		// this.pilot = new DifferentialPilot(WHEEL_DIAMETER, TRACK_WIDTH, leftMotor,
+		// rightMotor, false);
 
 		EV3ColorSensor colorS = new EV3ColorSensor(COLOR_SENSOR);
 		EV3TouchSensor touchS1 = new EV3TouchSensor(TOUCH_SENSOR_1);
@@ -54,129 +57,34 @@ public class Robot implements Runnable {
 
 		this.sensors = new SensorThread(color, touch1, touch2, distance);
 		this.missionMenu = new MissionMenu();
+		this.drive = new Drive(this);
+
 		new Thread(this.sensors).start();
 
 	}
 
 	public void mainLoop() throws InterruptedException {
-//		new Thread(this.sensors).start();
+		// new Thread(this.sensors).start();
 		new Thread(this).start(); // start the program
 		Button.LEDPattern(1); // green light
-		this.missionMenu.startGUI(this);
+		this.missionMenu.startGUI(this); // show menu on the brick's screen
 		System.exit(0);
 	}
 
 	@Override
 	public void run() {
 		// show mission menu on the brick's screen
-//		this.missionMenu = new MissionMenu();
+		// this.missionMenu = new MissionMenu();
 
 	}
-	
-	public void setSpeed(double speed) {
-		this.pilot.setTravelSpeed(speed);
-	}
 
-	public void goForwardWithPilot() {
-		this.pilot.forward();
-	}
-
-	public void goBackwardWithPilot() {
-		this.pilot.backward();
-	}
-	
-	public void goForwardWithMotors() {
-		this.leftMotor.forward();
-		this.rightMotor.forward();
-	}
-	
-	public void goBackwardWithMotors() {
-		this.leftMotor.backward();
-		this.rightMotor.backward();
-	}
-	
-	public void setMotorSpeed(float leftMotorTargetSpeed, float rightMotorTargetSpeed) {
-		this.leftMotor.setSpeed(leftMotorTargetSpeed);
-		this.rightMotor.setSpeed(rightMotorTargetSpeed);
-	}
-
-	/**
-	 * Moves the robot a specific distance (unit: cm) in an (hopefully) straight
-	 * line.
-	 * 
-	 * @param distance
-	 *            A positive distance causes forward motion, a negative distance
-	 *            moves backward.
-	 */
-	public void travel(double distance) {
-		this.pilot.travel(distance);
-	}
-
-	/**
-	 * Moves the robot a specified distance along an arc of specified radius, after
-	 * which the robot stops moving. The units (inches, cm) for distance must be the
-	 * same as the units used for radius.
-	 * 
-	 * @param radius
-	 *            If radius is positive, the robot arcs left, and the center of the
-	 *            turning circle is on the left side of the robot. If radius is
-	 *            negative, the robot arcs right, and the center of the turning
-	 *            circle is on the right side of the robot. If radius is zero, the
-	 *            robot rotates in place
-	 * @param distance
-	 *            The robot will stop when it has moved along the arc distance
-	 *            units. If distance is positive, the robot will move travel
-	 *            forwards. If distance is negative, the robot will move travel
-	 *            backwards. If distance is zero, the robot will not move and the
-	 *            method returns immediately.
-	 */
-	public void travelArc(double radius, double distance) {
-		this.pilot.travelArc(radius, distance, true);
-	}
-
-	public boolean isMoving() {
-		return this.pilot.isMoving();
-	}
-
-	public void stop() {
-		this.pilot.stop();
-	}
-
-	/**
-	 * 
-	 * @param angle
-	 *            The wanted angle of rotation in degrees. Positive angle rotate
-	 *            left (anti-clockwise), negative right.
-	 */
-	public void rotate(double angle) {
-		this.pilot.rotate(angle, true);
-	}
-
-	private void sleep(int millis) {
-		try {
-			Thread.sleep(millis);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
-
-//	/**
-//	 * Change the motors' speed in order to make the robot (hopefully) move along
-//	 * the line.
-//	 * <p>
-//	 * This method is specifically for mission Line-Following.
-//	 * </p>
-//	 * 
-//	 * @param turn
-//	 *            calculated by using proportional, integral and derivative
-//	 *            constants, <br>
-//	 * 			see {@link mission.LineFollowerThread}</br>
-//	 */
-//	public void changeMotorSpeed(float turn) {
-//		this.leftMotor.setSpeed((float) (this.pilot.getTravelSpeed() + turn));
-//		this.rightMotor.setSpeed((float) (this.pilot.getTravelSpeed() - turn));
-//	}
+	// private void sleep(int millis) {
+	// try {
+	// Thread.sleep(millis);
+	// } catch (InterruptedException e) {
+	// e.printStackTrace();
+	// }
+	// }
 
 	// ============= setters and getters ========================
 
@@ -196,14 +104,6 @@ public class Robot implements Runnable {
 		this.missionMenu = missionMenu;
 	}
 
-	public DifferentialPilot getPilot() {
-		return pilot;
-	}
-
-	public void setPilot(DifferentialPilot pilot) {
-		this.pilot = pilot;
-	}
-
 	public EV3LargeRegulatedMotor getLeftMotor() {
 		return leftMotor;
 	}
@@ -219,7 +119,21 @@ public class Robot implements Runnable {
 	public void setRightMotor(EV3LargeRegulatedMotor rightMotor) {
 		this.rightMotor = rightMotor;
 	}
-	
-	
+
+	public double getWHEEL_DIAMETER() {
+		return WHEEL_DIAMETER;
+	}
+
+	public double getTRACK_WIDTH() {
+		return TRACK_WIDTH;
+	}
+
+	public Drive getDrive() {
+		return drive;
+	}
+
+	public void setDrive(Drive drive) {
+		this.drive = drive;
+	}
 
 }
