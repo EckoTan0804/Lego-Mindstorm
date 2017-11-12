f4e22fe4d496c5ae53bcf17f74e7c0cf7e35e2c2
Chaoxiang <eggpainhorse@gmail.com>
2017-11-12 12:57:27 +0100

    forward with Motor

--- src/mission/LineFollowerThread.java
+++ src/mission/LineFollowerThread.java
@@ -1,5 +1,6 @@
 package mission;
 
+import ev3Robot.Drive;
 import ev3Robot.Robot;
 import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
@@ -25,7 +26,7 @@ public class LineFollowerThread implements Runnable {
 	 * controllers will try to get back to the line edge when it has drifted away
 	 * from it
 	 */
-	private final float Kp = 200f;
+	private final float Kp = 100f;
 
 	/*
 	 * the constant for the Integral controller
@@ -35,23 +36,25 @@ public class LineFollowerThread implements Runnable {
 	/*
 	 * the constant for the derivative controller
 	 */
-	private final float Kd = 100f;
+	private final float Kd = 50f;
 
 	/*
 	 * Target power level, power level of both motors when the robot is supposed to
 	 * go straight ahead, controls how fast the robot is moving along the line
 	 */
-	private final float Tp = 50;
+	private final float Tp = 400;
 
 	private float integral = 0f;
 	private float lastError = 0f;
 	private float derivative = 0f;
 
 	private Robot robot;
+	private Drive drive;
 
 	public LineFollowerThread(Robot robot) {
 		super();
 		this.robot = robot;
+		this.drive = robot.getDrive();
 	}
 
 	@Override
@@ -62,17 +65,19 @@ public class LineFollowerThread implements Runnable {
 
 	private void lineFollowing() {
 //		this.robot.
-		this.robot.goForward();
+//		this.drive.goForwardWithMotors();
 		while (Button.LEFT.isUp()) {
 			float sampleVal = this.robot.getSensors().getColor();
 			float error = sampleVal - offset;
 			integral = integral + error;
 			derivative = error - lastError;
 			float turn = Kp * error + Ki * integral + Kd * derivative;
-			this.robot.changeMotorSpeed(turn);
+			this.drive.setMotorSpeed(Tp + turn, Tp - turn);
+			this.drive.goForwardWithMotors();
 //			this.robot.goForward();
 			this.lastError = error;
 		}
+		this.drive.stopWithMotors();
 
 	}
 
