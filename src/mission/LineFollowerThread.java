a09435fc12c997d5432612a2e5edfa564c9c67e1
EckoTan0804 <ecko0804@gmail.com>
2017-11-12 14:57:48 +0100

    add comments in order to make the codes more readable

--- src/mission/LineFollowerThread.java
+++ src/mission/LineFollowerThread.java
@@ -2,7 +2,6 @@ package mission;
 
 import ev3Robot.Robot;
 import lejos.hardware.Button;
-import lejos.hardware.lcd.LCD;
 
 /**
  * For mission Line-Following a PID-controller will be used.
@@ -18,14 +17,20 @@ public class LineFollowerThread implements Runnable {
 	private final float BLACK = 0.05f;
 	private final float WHITE = 0.33f;
 
-	private float offset = WHITE - BLACK;
+	private float offset = (WHITE - BLACK) / 2;
+
+	/*
+	 * Target power level, power level of both motors when the robot is supposed to
+	 * go straight ahead, controls how fast the robot is moving along the line
+	 */
+	private final float Tp = 50;
 
 	/*
 	 * the constant for the Proportional controller, controls how fast the
 	 * controllers will try to get back to the line edge when it has drifted away
 	 * from it
 	 */
-	private final float Kp = 200f;
+	private final float Kp = (float) ((Tp - 0) / (0.14 - 0));
 
 	/*
 	 * the constant for the Integral controller
@@ -36,12 +41,6 @@ public class LineFollowerThread implements Runnable {
 	 * the constant for the derivative controller
 	 */
 	private final float Kd = 100f;
-
-	/*
-	 * Target power level, power level of both motors when the robot is supposed to
-	 * go straight ahead, controls how fast the robot is moving along the line
-	 */
-	private final float Tp = 50;
 
 	private float integral = 0f;
 	private float lastError = 0f;
@@ -56,22 +55,41 @@ public class LineFollowerThread implements Runnable {
 
 	@Override
 	public void run() {
-		LCD.drawString("HELLO", 0, 1);
 		this.lineFollowing();
 	}
 
 	private void lineFollowing() {
-//		this.robot.
-		this.robot.goForward();
+
+		this.robot.getDrive().setMotorSpeed(Tp, Tp);
+		this.robot.getDrive().goForwardWithMotors();
+
 		while (Button.LEFT.isUp()) {
+
+			// get the sample value measured by color sensor
 			float sampleVal = this.robot.getSensors().getColor();
+
+			// calculate turn, based on this value
 			float error = sampleVal - offset;
 			integral = integral + error;
 			derivative = error - lastError;
 			float turn = Kp * error + Ki * integral + Kd * derivative;
-			this.robot.changeMotorSpeed(turn);
-//			this.robot.goForward();
+
+			// adjust the power of left and right motors in order to make the robot follow
+			// the line
+			this.robot.getDrive().setMotorSpeed(this.robot.getLeftMotor().getTachoCount() - turn,
+					this.robot.getRightMotor().getTachoCount() + turn);
+
+			// robot goes forward
+			this.robot.getDrive().goForwardWithMotors();
+			
+			// update error
 			this.lastError = error;
+
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
