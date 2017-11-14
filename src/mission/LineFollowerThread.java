803c89a0787f36653df3f93008fb5068b8a32520
EckoTan0804 <ecko0804@gmail.com>
2017-11-14 20:56:38 +0100

    improved the code's structure

--- src/mission/LineFollowerThread.java
+++ src/mission/LineFollowerThread.java
@@ -34,7 +34,7 @@ public class LineFollowerThread implements Runnable {
 	 * controllers will try to get back to the line edge when it has drifted away
 	 * from it
 	 */
-	private final float Kp = (float) (((Tp - 0)/ (WHITE - offset)) * 1.8);
+	private final float Kp = (float) (((Tp - 0) / (WHITE - offset)) * 1.8);
 
 	/*
 	 * the constant for the Integral controller
@@ -80,92 +80,95 @@ public class LineFollowerThread implements Runnable {
 
 		while (Button.LEFT.isUp()) { // stop the routine and back to the main menu if LEFT is pressed
 
-			// get the sample value measured by color sensor
-			// this.robot.
-			// this.drive.goForwardWithMotors();
-				float leftTargetSpeed = 0;
-				float rightTargetSpeed = 0;
-				float error = 0;
-				float sampleVal = this.robot.getSensors().getColor();
-				
-				LCD.clear();
-				LCD.drawString(Mission.LINE_FOLLOWING.getMission(), 0, 0);
-				LCD.drawString("val = " + sampleVal, 0, 1);
-				//if (sampleVal < BLACK + EPS) {
-				//	this.robot.getDrive().stopWithMotors();
-				//	leftTargetSpeed = Tp;
-				//	rightTargetSpeed = Tp;
-				//} else 
-				if (sampleVal > WHITE - EPS) {
-					this.robot.getDrive().stopWithMotors();
-					leftTargetSpeed = -Tp;
-					rightTargetSpeed = Tp;
-				} else {
-					// calculate turn, based on this value
-					error = sampleVal - offset;
-					integral = integral + error;
-					derivative = error - lastError;
-					//float turn = Kp * error + Ki * integral + Kd * derivative;
-					float turn = Kp * error;
+			
+			float leftTargetSpeed = 0;
+			float rightTargetSpeed = 0;
+			float error = 0;
+			
+			// get the real time sample value measured by color sensor
+			float sampleVal = this.robot.getSensors().getColor();
 
-					// adjust the power of left and right motors in order to make the robot follow
-					// the line
-					leftTargetSpeed = Tp - turn;
-					rightTargetSpeed = Tp + turn;
-				}
-				LCD.drawString("L = " + leftTargetSpeed, 0, 2);
-				LCD.drawString("R = " + rightTargetSpeed, 0, 3);
-				//this.robot.getDrive().setMotorSpeed(leftTargetSpeed,
-				//		rightTargetSpeed);
-				
-				this.robot.getLeftMotor().startSynchronization();
-				this.robot.getRightMotor().startSynchronization();
-				if (leftTargetSpeed < 0) {
-					this.robot.getDrive().setLeftMotorSpeed(-leftTargetSpeed);
-					this.robot.getLeftMotor().backward();
-				} else {
-					this.robot.getDrive().setLeftMotorSpeed(leftTargetSpeed);
-					this.robot.getLeftMotor().forward();
-				}
-				if (rightTargetSpeed < 0) {
-					this.robot.getDrive().setRightMotorSpeed(-rightTargetSpeed);
-					this.robot.getRightMotor().backward();
-				} else {
-					this.robot.getDrive().setRightMotorSpeed(rightTargetSpeed);
-					this.robot.getRightMotor().forward();
-				}
-				this.robot.getLeftMotor().endSynchronization();
-				this.robot.getRightMotor().endSynchronization();
-				
+			LCD.clear();
+			LCD.drawString(Mission.LINE_FOLLOWING.getMission(), 0, 0);
+			LCD.drawString("val = " + sampleVal, 0, 1);
 
-				// robot goes forward
-//				this.robot.getDrive().goForwardWithMotors();
+			if (sampleVal > WHITE - EPS) { // special case: the robot need to do a 90 degree rotation
+				this.robot.getDrive().stopWithMotors();
+				leftTargetSpeed = -Tp;
+				rightTargetSpeed = Tp;
 
-				
+			} else { // normal case
 
-				// this.drive.setMotorSpeed(Tp + turn, Tp - turn);
-				// this.drive.goForwardWithMotors();
-				//// this.robot.goForward();
+				// calculate turn, based on the sample value
+				error = sampleVal - offset;
+				integral = integral + error;
+				derivative = error - lastError;
+				// float turn = Kp * error + Ki * integral + Kd * derivative;
+				float turn = Kp * error; // only a P-controller will be used.
 
-				// update error
-				this.lastError = error;
-
-				try {
-					Thread.sleep(20);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+				// adjust the power of left and right motors in order to make the robot follow
+				// the line
+				leftTargetSpeed = Tp - turn;
+				rightTargetSpeed = Tp + turn;
 			}
 
-			Sound.beepSequence();
-			this.drive.stopWithMotors();
+			// print the target speed of left and right motors on the brick's screen
+			LCD.drawString("L = " + leftTargetSpeed, 0, 2);
+			LCD.drawString("R = " + rightTargetSpeed, 0, 3);
+
+			this.adjustRobotMovement(this.robot, leftTargetSpeed, rightTargetSpeed);
+
+			// this.robot.getLeftMotor().startSynchronization();
+			// this.robot.getRightMotor().startSynchronization();
+			// if (leftTargetSpeed < 0) {
+			// this.robot.getDrive().setLeftMotorSpeed(-leftTargetSpeed);
+			// this.robot.getLeftMotor().backward();
+			// } else {
+			// this.robot.getDrive().setLeftMotorSpeed(leftTargetSpeed);
+			// this.robot.getLeftMotor().forward();
+			// }
+			// if (rightTargetSpeed < 0) {
+			// this.robot.getDrive().setRightMotorSpeed(-rightTargetSpeed);
+			// this.robot.getRightMotor().backward();
+			// } else {
+			// this.robot.getDrive().setRightMotorSpeed(rightTargetSpeed);
+			// this.robot.getRightMotor().forward();
+			// }
+			// this.robot.getLeftMotor().endSynchronization();
+			// this.robot.getRightMotor().endSynchronization();
+
+			// update error
+			this.lastError = error;
+
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		Sound.beepSequence();
+		this.drive.stopWithMotors();
 	}
-	
-//	private boolean isGeneral(float sampleVal) {
-//		int rotateAngle = 0;
-//		if (sampleVal <= BLACK) {
-//			this.robot.getDrive().stopWithMotors();
-//			
-//		}
-//	}
+
+	private void adjustRobotMovement(Robot robot, float leftTargetSpeed, float rightTargetSpeed) {
+		robot.getLeftMotor().startSynchronization();
+		robot.getRightMotor().startSynchronization();
+		if (leftTargetSpeed < 0) {
+			robot.getDrive().setLeftMotorSpeed(-leftTargetSpeed);
+			robot.getLeftMotor().backward();
+		} else {
+			robot.getDrive().setLeftMotorSpeed(leftTargetSpeed);
+			robot.getLeftMotor().forward();
+		}
+		if (rightTargetSpeed < 0) {
+			robot.getDrive().setRightMotorSpeed(-rightTargetSpeed);
+			robot.getRightMotor().backward();
+		} else {
+			robot.getDrive().setRightMotorSpeed(rightTargetSpeed);
+			robot.getRightMotor().forward();
+		}
+		robot.getLeftMotor().endSynchronization();
+		robot.getRightMotor().endSynchronization();
+	}
 }
