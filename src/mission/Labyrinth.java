26c3e38f248ce3621bed8508e7cc4739ce7f4196
EckoTan0804 <ecko0804@gmail.com>
2017-11-20 22:18:32 +0100

    create Labyrinth.java

---
+++ src/mission/Labyrinth.java
@@ -0,0 +1,18 @@
+package mission;
+
+import ev3Robot.Robot;
+
+public class Labyrinth {
+	
+	private Robot robot;
+
+	public Labyrinth(Robot robot) {
+		super();
+		this.robot = robot;
+	}
+	
+	public void startLabyrinth() {
+		
+	}
+
+}
